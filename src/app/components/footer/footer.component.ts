592b20a9ea10651f823e3107e7704534c0e57a9c
Victor Rivero <frovic.ve@gmail.com>
2017-12-10 21:39:04 -0400

    Header. Body, Footer Listo

---
+++ src/app/components/footer/footer.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: []
+})
+export class FooterComponent {
+
+anio:number = new Date().getFullYear();
+  
+
+
+}
